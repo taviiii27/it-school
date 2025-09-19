7ea81965ede73012214a43d44d25dc095c1f53b2
taviiii27 <petrucretu03@gmail.com>
2025-09-19 23:20:31 +0300

    "homework from previous weeks"

--- src/com.package.session11/homework-8.09.2025/ShoppingListArray.java
+++
@@ -1,9 +0,0 @@
-public class ShoppingListArray {
-    public static void main(String[] args) {
-        String[] shoppingList = {"Milk", "Bread", "Eggs", "Cheese", "Butter"};
-        System.out.println("Shopping List:");
-        for (String item : shoppingList) {
-            System.out.println(item);
-        }
-    }
-}
